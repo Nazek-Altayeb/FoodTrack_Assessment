04e4a1c40a9d46f494c59f7835e5ceb10d3aaaac
Sheefoo <n.almalaika@yahoo.com>
2023-12-16 18:25:50 +0100

    register both OpeningHours and Food in  admin.py

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Store, Address
+from .models import Store, Address, OpeningHours, Food
 
 
 admin.site.register(Store)
 admin.site.register(Address)
+admin.site.register(OpeningHours)
+admin.site.register(Food)
