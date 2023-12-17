3746ac74a6988676de1baee564b05547bd097507
Sheefoo <n.almalaika@yahoo.com>
2023-12-17 23:30:58 +0100

    extend the update method to consider the nested serializers Foods and OpeningHours

--- store/serializers.py
+++ store/serializers.py
@@ -52,31 +52,78 @@ class StoreSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         address_data = validated_data.pop('address')
         openingHours_data = validated_data.pop('openingHours')
-        food_data = validated_data.pop('foods')
+        foods = validated_data.pop('foods')
         address = Address.objects.create(**address_data)
         new_store = Store.objects.create(**validated_data, address=address)
         for openingHour in openingHours_data:
             OpeningHours.objects.create(**openingHour, store = new_store)
-        for food_item in food_data:
+        for food_item in foods:
             Foods.objects.create(**food_item, store = new_store)    
 
         return new_store
     
 
     def update(self, instance, validated_data):
+        foods = validated_data.pop('foods')
         address_data = validated_data.pop('address')
-        food_data = validated_data.pop('foods')
-        openingHour_data = validated_data.pop('openingHours')
+        openingHours = validated_data.pop('openingHours')
         address_id = address_data.get('id')
         instance.name = validated_data.get('name', instance.name)
-        instance.openingHours = validated_data.get('openingHours', instance.openingHours)
         instance.save()
-        
+
+        """
+        Update the address details for specific store
+        """
         existing_address = Address.objects.get(pk=address_id)
-        
         existing_address.city = address_data.get('city', existing_address.city)
         existing_address.state = address_data.get('state', existing_address.state)
         existing_address.zipcode = address_data.get('zipcode', existing_address.zipcode)
         existing_address.save()
+
+        """
+        Update the foods objects for specific store
+        """
+        kept_foods = []
+        for food in foods:
+            if "id" in food.keys():
+                if Foods.objects.filter(id=food["id"]).exists():
+                    c = Foods.objects.get(id=food["id"])
+                    c.foodName = food.get('foodName', c.foodName)
+                    c.salesPerDay = food.get('salesPerDay', c.salesPerDay)
+                    c.returnedItemsPerDay = food.get('returnedItemsPerDay', c.returnedItemsPerDay)
+                    c.save()
+                    kept_foods.append(c.id)
+                else:
+                    continue
+            else:
+                c = Foods.objects.create(**food, store=instance)
+                kept_foods.append(c.id)
+
+        """for food in instance.foods:
+            if food.id not in kept_foods:
+                food.delete()"""
+
+        """
+        Update the opening hours objects for specific store
+        """
+        kept_openingHours = []
+        for openingHour in openingHours:
+            if "id" in openingHour.keys():
+                if OpeningHours.objects.filter(id=openingHour["id"]).exists():
+                    c = OpeningHours.objects.get(id=openingHour["id"])
+                    c.day = openingHour.get('day', c.day)
+                    c.time = openingHour.get('time', c.time)
+                    c.save()
+                    kept_openingHours.append(c.id)
+                else:
+                    continue
+            else:
+                c = OpeningHours.objects.create(**openingHour, store=instance)
+                kept_openingHours.append(c.id)
+
+        """for openingHour in instance.openingHours:
+            if openingHour.id not in kept_openingHours:
+                openingHour.delete()"""
+
 
         return instance
